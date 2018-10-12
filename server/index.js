a2eaadd73087f6fef57ee1176f2188809d13b545
sumitkumar1207 <logsumit28@gmail.com>
2018-10-12 19:10:47 +0530

    Connectted backend to Vue frontend. And getting all the posts

--- server/index.js
+++ server/index.js
@@ -5,12 +5,21 @@ const posts = require("./routes/api/posts");
 const app = express();
 
 // Body-parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //Use routes
 app.use("/api/posts", posts);
 app.use(cors());
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 const port = process.env.Port || 5000;
 
