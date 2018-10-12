b314e81066003e0bdd1ad55e3e3eafa0fb968506
sumitkumar1207 <logsumit28@gmail.com>
2018-10-12 13:18:40 +0530

    Adding the GET and POST routes to the app.

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -3,12 +3,43 @@ const mongodb = require("mongodb");
 const keys = require("../../config/kyes");
 const router = express.Router();
 
-//Get Posts
-router.get("/", (req, res) => {
-  res.send("hello");
+//test route
+router.get("/test", (req, res) => {
+  res.status(200).json({ msg: "Test route working!" });
 });
+
+//Get Posts
+router.get("/", async (req, res) => {
+  const posts = await loadPostCollection();
+  res.send(await posts.find({}).toArray());
+});
+
 //Add posts
+router.post("/", async (req, res) => {
+  const posts = await loadPostCollection();
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date()
+  });
+  res.status(201).send({ msg: "Your post created successfully!" });
+});
 
 //Delete Posts
+router.delete("/:id", async (req, res) => {
+  const posts = await loadPostCollection();
+  await posts.deleteOne({
+    _id: new mongodb.ObjectID(req.params.id)
+  });
+  res.status(200).send({ msg: "Your post have deleted successfully" });
+});
 
+async function loadPostCollection() {
+  const client = await mongodb.MongoClient.connect(
+    keys.mongoURI,
+    {
+      useNewUrlParser: true
+    }
+  );
+  return client.db("vue-app").collection("posts");
+}
 module.exports = router;
